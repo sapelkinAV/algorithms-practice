75a01c6417f0727ef94ed45b728dc1b0a6a3cfc9
sapelkinAV <al.vi.sapelkin@gmail.com>
2022-07-07 18:34:13 +0400

    add rotated binarysearch

--- src/main/java/com/sapelkinav/Main.java
+++ src/main/java/com/sapelkinav/Main.java
@@ -1,14 +1,11 @@
 package com.sapelkinav;
 
-import com.sapelkinav.binary_search.BadVersions;
-import com.sapelkinav.binary_search.RotatedIntegerBinarySearch;
-import com.sapelkinav.binary_search.SquareRootBinarySearch;
-import com.sapelkinav.dynamicprogramming.FibonacciMemorization;
+import com.sapelkinav.binary_search.RotatedIntegerFindMinimum;
 
 public class Main {
     public static void main(String[] args) {
        // BinarySearch.runBinarySearch();
-        var algorithm = new BadVersions();
+        var algorithm = new RotatedIntegerFindMinimum();
         algorithm.runAlgorithm();
 
     }
--- src/main/java/com/sapelkinav/binary_search/BadVersions.java
+++ src/main/java/com/sapelkinav/binary_search/BadVersions.java
@@ -6,31 +6,31 @@ public class BadVersions implements AlgorithmRunner {
     @Override
     public void runAlgorithm() {
         var result = firstBadVersion(10);
-        System.out.println("first bad version is "+ result);
+        System.out.println("first bad version is " + result);
     }
 
     public int firstBadVersion(int n) {
         int left = 0, pivot, right = n;
         while (left <= right) {
-            pivot = left + (right - left)/2;
+            pivot = left + (right - left) / 2;
             var isBad = isBadVersion(pivot);
-            if(isBad) {
-                if(!isBadVersion(pivot -1)){
+            if (isBad) {
+                if (!isBadVersion(pivot - 1)) {
                     return pivot;
                 }
-                right = pivot -1;
+                right = pivot - 1;
             } else {
-                if(isBadVersion(pivot + 1)){
-                    return pivot +1;
+                if (isBadVersion(pivot + 1)) {
+                    return pivot + 1;
                 }
-                left = pivot +2;
+                left = pivot + 2;
             }
         }
         return -1;
 
     }
 
-    boolean isBadVersion(int guess){
+    boolean isBadVersion(int guess) {
         return guess >= 7;
     }
 }
